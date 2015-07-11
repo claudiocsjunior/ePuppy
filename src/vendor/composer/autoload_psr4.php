070adafaa63baedfeabe85489a7a60dfc7eae2b9
diegosilveiracn <diegoscnascimento@gmail.com>
2015-07-11 12:27:57 -0300

    Configuração do banco de dados

--- src/vendor/composer/autoload_psr4.php
+++ src/vendor/composer/autoload_psr4.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'XdgBaseDir\\' => array($vendorDir . '/dnoegel/php-xdg-base-dir/src'),
     'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
+    'Symfony\\Component\\VarDumper\\' => array($vendorDir . '/symfony/var-dumper'),
     'Symfony\\Component\\Filesystem\\' => array($vendorDir . '/symfony/filesystem'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'Symfony\\Component\\Config\\' => array($vendorDir . '/symfony/config'),
